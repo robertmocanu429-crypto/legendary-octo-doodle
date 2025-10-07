196d5ba6450c8e606f098f3489c7ac27cf6221f9
robertmocanu429-crypto <robertmocanu429@gmail.com>
2025-10-07 09:13:49 +0000

    Lösche Aufgabe02_2

--- test.java
+++ test.java
@@ -1,1 +1,21 @@
-fhndunhfnun
+/* Beispiel: Berechnung zweier Zahlen */
+public class test {
+    public static void main (String[] args){
+
+        //Speicherplatz
+        int summe;
+        int a;
+        int b;
+
+        //EIngabe
+        a = 7;
+        b = 13;
+
+        //Summe
+        summe = a + b;
+
+        //Output
+        System.out.print (a + " + " + b + " ergibt ");
+        System.out.println(summe);
+    }
+}
